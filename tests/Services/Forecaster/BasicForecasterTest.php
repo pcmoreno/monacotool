958bf53e8083f76201ad5ed34e03b2f8dcd05351
pcmoreno <pcmoreno@yahoo.com>
2026-01-25 23:41:20 +0100

    some upgrades

---
+++ tests/Services/Forecaster/BasicForecasterTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Services\Forecaster;
+
+use App\Services\Forecaster\BasicForecaster;
+use PHPUnit\Framework\TestCase;
+
+class BasicForecasterTest extends TestCase
+{
+
+}
